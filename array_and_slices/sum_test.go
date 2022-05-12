d9b6fe3a8e1d84c60a2896c5d59b252d8b7988ab
Maximiliano Greco <mmngreco@gmail.com>
2022-05-12 19:09:09 +0200

    tst(sum): create assert function

--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -33,21 +33,24 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAllTail(t *testing.T) {
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTail([]int{1, 2}, []int{0, 9})
-		want := []int{2, 9}
 
+    checkSums := func (t testing.TB, got, want []int) {
+        t.Helper()
 		if !reflect.DeepEqual(got, want) {
 			t.Errorf("got %v want %v", got, want)
 		}
+    }
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+        checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTail([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
+        checkSums(t, got, want)
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
 	})
 }
