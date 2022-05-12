e170ac5a4a60725fd417d3bba8b4f898d4ae7cec
Maximiliano Greco <mmngreco@gmail.com>
2022-05-12 18:58:11 +0200

    bug: make it works with empty slices

--- array_and_slices/sum.go
+++ array_and_slices/sum.go
@@ -22,8 +22,14 @@ func Sum(numbers []int) (total int) {
 func SumAllTail(numbersToSum ...[]int) (sums []int) {
 
 	for _, numbers := range numbersToSum {
-        tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+
+        if len(numbers) == 0 {
+            sums = append(sums, 0)
+        } else {
+            tail := numbers[1:]
+            sums = append(sums, Sum(tail))
+        }
+
 	}
 	return sums
 }
--- array_and_slices/sum_test.go
+++ array_and_slices/sum_test.go
@@ -33,11 +33,21 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAllTail(t *testing.T) {
-	got := SumAllTail([]int{1, 2}, []int{0, 9})
-	want := []int{2, 9}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTail([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
