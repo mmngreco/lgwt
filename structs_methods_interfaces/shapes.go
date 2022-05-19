c53c3d6dd1e9d3a99e645661a960271f8de8a307
Maximiliano Greco <mmngreco@gmail.com>
2022-05-19 20:35:13 +0200

    working on structs methods and interfaces

---
+++ structs_methods_interfaces/shapes.go
@@ -0,0 +1,28 @@
+package shapes
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+    Area() float64
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
---
+++ structs_methods_interfaces/shapes_test.go
@@ -0,0 +1,39 @@
+package shapes
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := rectangle.Perimeter()
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+    checkArea := func(t testing.TB, shape Shape, want float64) {
+        t.Helper()
+        got := shape.Area()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+
+    }
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+        checkArea(t, rectangle, 72.0)
+
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+        checkArea(t, circle, 314.1592653589793)
+
+	})
+
+}
